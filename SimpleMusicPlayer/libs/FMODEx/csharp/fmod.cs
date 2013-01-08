f8bb32cfe224b410c7e1cba7ffdbfac56bd3222f
Jan Karger <punkerat76@jkarger.de>
2013-01-08 23:43:25 +0100

    use latest fmod dll v4.44.03
    * change fmod sources and dll to linked files

--- SimpleMusicPlayer/libs/FMODEx/csharp/fmod.cs
+++ SimpleMusicPlayer/libs/FMODEx/csharp/fmod.cs
@@ -16,7 +16,7 @@ namespace FMOD
     */
     public class VERSION
     {
-        public const int    number = 0x00044402;
+        public const int    number = 0x00044403;
 #if WIN64
         public const string dll    = "fmodex64";
 #else
