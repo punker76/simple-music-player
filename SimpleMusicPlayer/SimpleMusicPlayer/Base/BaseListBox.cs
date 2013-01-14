d0c385c84af4305e41da6eb30cd42f8d824a0180
Jan Karger <punkerat76@jkarger.de>
2013-01-14 22:20:29 +0100

    BaseListBox without Space Keydown

---
+++ SimpleMusicPlayer/SimpleMusicPlayer/Base/BaseListBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SimpleMusicPlayer.Base
+{
+  public class BaseListBox : ListBox
+  {
+    protected override void OnKeyDown(System.Windows.Input.KeyEventArgs e) {
+      if (!this.IsTextSearchEnabled && e.Key == Key.Space) {
+        e.Handled = false;
+      } else {
+        base.OnKeyDown(e);
+      }
+    }
+  }
+}
