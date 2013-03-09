e616a50afd4074ca3c970970a627b86e4811963f
Jan Karger <punkerat76@jkarger.de>
2013-03-09 23:34:57 +0100

    use latest MahApps.Metro.0.10.1.30-ALPHA

--- SimpleMusicPlayer/SimpleMusicPlayer/Base/AttachedCommandBehavior.cs
+++ SimpleMusicPlayer/SimpleMusicPlayer/Base/AttachedCommandBehavior.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Windows;
 using System.Windows.Input;
 
