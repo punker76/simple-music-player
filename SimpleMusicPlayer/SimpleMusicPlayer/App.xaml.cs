6e305e31486c717cdf7b77c00a6c4566e284700b
Jan Karger <punkerat76@jkarger.de>
2013-03-29 21:56:17 +0100

    use Program.cs to load assemblies

--- SimpleMusicPlayer/SimpleMusicPlayer/App.xaml.cs
+++ SimpleMusicPlayer/SimpleMusicPlayer/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Reflection;
-using System.Windows;
+﻿using System.Windows;
 
 namespace SimpleMusicPlayer
 {
@@ -9,19 +7,5 @@ namespace SimpleMusicPlayer
   /// </summary>
   public partial class App : Application
   {
-    protected override void OnStartup(StartupEventArgs e) {
-      AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
-        String resourceName = "SimpleMusicPlayer.DllsAsResource." + new AssemblyName(args.Name).Name + ".dll";
-        using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)) {
-          if (stream != null) {
-            Byte[] assemblyData = new Byte[stream.Length];
-            stream.Read(assemblyData, 0, assemblyData.Length);
-            return Assembly.Load(assemblyData);
-          }
-        }
-        return null;
-      };
-      base.OnStartup(e);
-    }
   }
 }
---
+++ SimpleMusicPlayer/SimpleMusicPlayer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace SimpleMusicPlayer
+{
+  public class Program
+  {
+    [STAThread]
+    public static void Main() {
+      AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
+                                                   var resourceName = "SimpleMusicPlayer.DllsAsResource." + new AssemblyName(args.Name).Name + ".dll";
+                                                   using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)) {
+                                                     if (stream != null) {
+                                                       var assemblyData = new Byte[stream.Length];
+                                                       stream.Read(assemblyData, 0, assemblyData.Length);
+                                                       return Assembly.Load(assemblyData);
+                                                     }
+                                                   }
+                                                   return null;
+                                                 };
+      App.Main();
+    }
+  }
+}
