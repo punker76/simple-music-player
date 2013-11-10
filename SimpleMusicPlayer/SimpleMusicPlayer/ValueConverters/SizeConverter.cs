872249aad14daa2c0351fbaa3b3bf9dfc7a8a147
punker76 <punkerat76@gmail.com>
2013-11-10 23:38:33 +0100

    flexible ui for play info and control

---
+++ SimpleMusicPlayer/SimpleMusicPlayer/ValueConverters/SizeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SimpleMusicPlayer.ValueConverters
+{
+  public class SizeConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      if (value is double) {
+        var actualSize = (double)value;
+        if (actualSize > 800) return "Large";
+        if (actualSize > 600) return "Medium";
+      }
+      return "Small";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+      return DependencyProperty.UnsetValue;
+    }
+  }
+}
