111c3b7efb1c2652fd4fe68c32d61b105e5a5b89
Jan Karger <punkerat76@gmail.com>
2014-04-12 13:39:53 +0200

    build batch for release

---
+++ SimpleMusicPlayer/Build.cmd
@@ -0,0 +1,5 @@
+@echo on
+call "%VS100COMNTOOLS%vsvars32.bat"
+
+msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform=x86
+msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform="Any CPU"
