c7926e963388b4bc95312fd75b129be358133f49
Jan Karger <punkerat76@gmail.com>
2014-05-26 14:50:14 +0200

    minor cosmetics, remove boilerplate code
    - add MediaFileTransitioningContentControl
    - don't use opacity for textblocks or their brushes, it brakes the clear
    type system
    - add SemiTextColorBrush used for additional title information
    - add SemiTextColorBrush2 used for play list index
    - use RenderOptions and TextOptions at position slider

---
+++ SimpleMusicPlayer/SimpleMusicPlayer/Views/MediaFileTransitioningContentControl.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MahApps.Metro.Controls;
+
+namespace SimpleMusicPlayer.Views
+{
+  public partial class MediaFileTransitioningContentControl : TransitioningContentControl
+  {
+    public MediaFileTransitioningContentControl()
+    {
+      InitializeComponent();
+    }
+  }
+}
