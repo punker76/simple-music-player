4357e3e839114f07810e53e6dfd6d7b6154a6ec6
Jan Karger <punkerat76@gmail.com>
2014-06-20 18:35:22 +0200

    update fmod to 4.44.32

--- SimpleMusicPlayer/libs/FMODEx/csharp/fmod.cs
+++ SimpleMusicPlayer/libs/FMODEx/csharp/fmod.cs
@@ -16,7 +16,7 @@ namespace FMOD
     */
     public class VERSION
     {
-        public const int    number = 0x00044432;
+        public const int    number = 0x00044436;
 #if WIN64
         public const string dll    = "fmodex64";
 #else
