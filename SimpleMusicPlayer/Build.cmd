a04f9f54521a9e2e6df6dd5e2d7225c994d73541
Jan Karger <punkerat76@gmail.com>
2014-06-20 18:35:44 +0200

    add /m to build

--- SimpleMusicPlayer/Build.cmd
+++ SimpleMusicPlayer/Build.cmd
@@ -1,5 +1,5 @@
 @echo on
 call "%VS120COMNTOOLS%vsvars32.bat"
 
-msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform=x86
-msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform="Any CPU"
+msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform=x86 /m
+msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform="Any CPU" /m
