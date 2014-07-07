254f78955eb67fd6bc5241737623ad55ae22f21e
Jan Karger <punkerat76@gmail.com>
2014-07-07 15:41:08 +0200

    add SelectorScrollViewerAttachedBehavior
    with this behavior its now possible to set the scrolling lines for the
    scrollviewer

---
+++ SimpleMusicPlayer/SimpleMusicPlayer/Base/SelectorScrollViewerAttachedBehavior.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace SimpleMusicPlayer.Base
+{
+  public class SelectorScrollViewerAttachedBehavior
+  {
+    public static readonly DependencyProperty ScrollingLinesProperty
+      = DependencyProperty.RegisterAttached("ScrollingLines",
+                                            typeof(int),
+                                            typeof(SelectorScrollViewerAttachedBehavior),
+                                            new UIPropertyMetadata(3, OnScrollingLinesPropertyChangedCallback));
+
+    public static readonly DependencyProperty ScrollViewerProperty
+      = DependencyProperty.RegisterAttached("ScrollViewer",
+                                            typeof(ScrollViewer),
+                                            typeof(SelectorScrollViewerAttachedBehavior),
+                                            new UIPropertyMetadata(null));
+
+    public static int GetScrollingLines(DependencyObject source)
+    {
+      return (int)source.GetValue(ScrollingLinesProperty);
+    }
+
+    public static void SetScrollingLines(DependencyObject source, int value)
+    {
+      source.SetValue(ScrollingLinesProperty, value);
+    }
+
+    public static ScrollViewer GetScrollViewer(DependencyObject source)
+    {
+      return (ScrollViewer)source.GetValue(ScrollViewerProperty);
+    }
+
+    public static void SetScrollViewer(DependencyObject source, ScrollViewer value)
+    {
+      source.SetValue(ScrollViewerProperty, value);
+    }
+
+    private static void OnScrollingLinesPropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+    {
+      var lb = dependencyObject as Selector;
+      if (lb != null && e.NewValue != e.OldValue && e.NewValue is int) {
+        lb.Loaded -= OnSelectorLoaded;
+        lb.Loaded += OnSelectorLoaded;
+      }
+    }
+
+    private static void OnSelectorLoaded(object sender, RoutedEventArgs routedEventArgs)
+    {
+      var lb = sender as Selector;
+      if (lb != null) {
+        // get or store scrollviewer
+        SetScrollViewer(lb, lb.GetDescendantByType(typeof(ScrollViewer)) as ScrollViewer);
+        lb.PreviewMouseWheel -= OnSelectorPreviewMouseWheel;
+        lb.PreviewMouseWheel += OnSelectorPreviewMouseWheel;
+      }
+    }
+
+    private static void OnSelectorPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+      var lb = sender as Selector;
+      if (lb != null) {
+        // get or store scrollviewer
+        var lbScrollViewer = GetScrollViewer(lb);
+        if (lbScrollViewer != null) {
+          var scrollingLines = GetScrollingLines(lb);
+          if (e.Delta < 0) {
+            for (var i = 0; i < scrollingLines; i++) {
+              lbScrollViewer.LineDown();
+            }
+          } else {
+            for (var i = 0; i < scrollingLines; i++) {
+              lbScrollViewer.LineUp();
+            }
+          }
+          e.Handled = true;
+        }
+      }
+    }
+  }
+}
---
+++ SimpleMusicPlayer/SimpleMusicPlayer/Base/VisualExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SimpleMusicPlayer.Base
+{
+  public static class VisualExtensions
+  {
+    public static Visual GetDescendantByType(this Visual element, Type type)
+    {
+      if (element == null) {
+        return null;
+      }
+      if (element.GetType() == type) {
+        return element;
+      }
+      Visual foundElement = null;
+      if (element is FrameworkElement) {
+        (element as FrameworkElement).ApplyTemplate();
+      }
+      for (var i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++) {
+        var visual = VisualTreeHelper.GetChild(element, i) as Visual;
+        foundElement = GetDescendantByType(visual, type);
+        if (foundElement != null) {
+          break;
+        }
+      }
+      return foundElement;
+    }
+  }
+}
