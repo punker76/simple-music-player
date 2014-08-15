1ca96c6ed60fd654537dfcba702a8f8032991574
Jan Karger <punkerat76@gmail.com>
2014-08-15 22:10:07 +0200

    update build batch

--- SimpleMusicPlayer/Build.cmd
+++ SimpleMusicPlayer/Build.cmd
@@ -1,5 +1,5 @@
 @echo on
 call "%VS120COMNTOOLS%vsvars32.bat"
 
-msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform=x86 /m
-msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform="Any CPU" /m
+msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform=x86 /m /t:Clean,Rebuild
+msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform="Any CPU" /m /t:Clean,Rebuild
