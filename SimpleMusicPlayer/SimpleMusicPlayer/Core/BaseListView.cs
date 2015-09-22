a6869b2b29b9e07fb57b788f1da5ad8f1e518a09
Jan Karger <punkerat76@gmail.com>
2015-09-22 23:14:09 +0200

    next ListView step

--- SimpleMusicPlayer/SimpleMusicPlayer/Core/BaseListView.cs
+++ SimpleMusicPlayer/SimpleMusicPlayer/Core/BaseListView.cs
@@ -1,13 +1,141 @@
 using System;
 using System.ComponentModel;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
+using System.Windows.Threading;
+using MahApps.Metro.Controls;
+using ReactiveUI;
+using Splat;
 
 namespace SimpleMusicPlayer.Core
 {
-    public class BaseListView : ListView
+    public class BaseListView : ListView, IEnableLogger
+    {
+        public static readonly DependencyProperty ScrollViewerProperty = DependencyProperty.Register(
+            "ScrollViewer", typeof(ScrollViewer), typeof(BaseListView), new PropertyMetadata(default(ScrollViewer)));
+
+        public ScrollViewer ScrollViewer
+        {
+            get { return (ScrollViewer)GetValue(ScrollViewerProperty); }
+            set { SetValue(ScrollViewerProperty, value); }
+        }
+
+        public static readonly DependencyProperty VerticalScrollBarWidthProperty = DependencyProperty.Register(
+            "VerticalScrollBarWidth", typeof(double), typeof(BaseListView), new PropertyMetadata(0d));
+
+        public double VerticalScrollBarWidth
+        {
+            get { return (double)GetValue(VerticalScrollBarWidthProperty); }
+            set { SetValue(VerticalScrollBarWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty ObserveItemContainerGeneratorProperty
+            = DependencyProperty.Register("ObserveItemContainerGenerator",
+                                          typeof(bool),
+                                          typeof(BaseListView),
+                                          new PropertyMetadata(default(bool)));
+
+        public bool ObserveItemContainerGenerator
+        {
+            get { return (bool)this.GetValue(ObserveItemContainerGeneratorProperty); }
+            set { this.SetValue(ObserveItemContainerGeneratorProperty, value); }
+        }
+
+        public static readonly DependencyProperty ScrollIndexProperty
+            = DependencyProperty.Register("ScrollIndex",
+                                          typeof(int),
+                                          typeof(BaseListView),
+                                          new PropertyMetadata(-1));
+
+        public int ScrollIndex
+        {
+            get { return (int)this.GetValue(ScrollIndexProperty); }
+            set { this.SetValue(ScrollIndexProperty, value); }
+        }
+
+        public BaseListView()
+        {
+            this.WhenAnyValue(x => x.Items)
+                .CombineLatest(this.WhenAnyValue(x => x.ScrollIndex), (items, index) => items != null && items.Count > index && index >= 0)
+                .Subscribe(canScroll => {
+                    if (canScroll)
+                    {
+                        Action scrollAction = () => {
+                            this.Log().Debug("scroll into view for {0}", this.ScrollIndex);
+                            this.ScrollIntoView(this.Items[this.ScrollIndex]);
+                        };
+                        this.Dispatcher.BeginInvoke(DispatcherPriority.Background, scrollAction);
+                    }
+                });
+
+            this.Events().Loaded.Subscribe(e => {
+                var observeItemContGenerator = this.ObservableForProperty(x => x.ObserveItemContainerGenerator)
+                                                   .Where(x => x.Value == true)
+                                                   .Select(_ => Unit.Default);
+                var itemContGeneratorStatusChanged = this.ItemContainerGenerator.Events().StatusChanged
+                                                         .Throttle(TimeSpan.FromMilliseconds(150), RxApp.MainThreadScheduler)
+                                                         .Select(_ => Unit.Default);
+                Observable.Zip(observeItemContGenerator, itemContGeneratorStatusChanged).Subscribe(_ => this.FocusSelectedItem());
+            });
+        }
+
+        private void FocusSelectedItem()
+        {
+            if (this.ItemContainerGenerator.Status != GeneratorStatus.ContainersGenerated) return;
+            this.Log().Debug("ItemContainerGenerator status changed");
+            if (this.Items.Count == 0) return;
+            var index = this.SelectedIndex;
+            if (index < 0) return;
+            Action focusAction = () => {
+                this.ObserveItemContainerGenerator = false;
+                if (this.Items.Count == 0) return;
+                index = this.SelectedIndex;
+                if (index < 0) return;
+                this.Focus();
+                this.ScrollIntoView(this.SelectedItem);
+                var item = this.ItemContainerGenerator.ContainerFromIndex(index) as ListViewItem;
+                if (item == null) return;
+                item.Focus();
+                this.Log().Debug("focus selected item {0} / {1}", index, item);
+            };
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Background, focusAction);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this.GetTheScrollViewer();
+        }
+
+        private void GetTheScrollViewer()
+        {
+            if (this.ScrollViewer == null)
+            {
+                this.ScrollViewer = this.GetDescendantByType(typeof(ScrollViewer)) as ScrollViewer;
+                if (this.ScrollViewer != null)
+                {
+                    this.ScrollViewer.Loaded += ScrollViewer_Loaded;
+                }
+            }
+        }
+
+        void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            var scrollViewer = (ScrollViewer)sender;
+            var scrollBar = scrollViewer.FindChild<ScrollBar>("PART_VerticalScrollBar");
+            if (scrollBar != null)
+            {
+                VerticalScrollBarWidth = scrollBar.Width;
+            }
+        }
+    }
+
+    public class AutoGeneratedListView : BaseListView
     {
         public Type DataType { get; set; }
 
@@ -24,7 +152,7 @@ namespace SimpleMusicPlayer.Core
             }
         }
 
-        private static void CreateColumns(BaseListView lv)
+        private static void CreateColumns(AutoGeneratedListView lv)
         {
             var gridView = new GridView { AllowsColumnReorder = true };
 
