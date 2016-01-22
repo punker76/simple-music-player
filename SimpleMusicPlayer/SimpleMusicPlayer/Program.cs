c127f229103d1aae155628351ca8823d43d783cd
Jan Karger <punkerat76@gmail.com>
2016-01-22 22:08:58 +0100

    do the Fody way

--- SimpleMusicPlayer/SimpleMusicPlayer/Program.cs
+++ SimpleMusicPlayer/SimpleMusicPlayer/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.Remoting;
 using Microsoft.Shell;
 
@@ -8,23 +7,6 @@ namespace SimpleMusicPlayer
     public class Program
     {
         private const int MAXTRIES = 10;
-
-        static Program()
-        {
-            var executingAssembly = Assembly.GetExecutingAssembly();
-            var executingAssemblyName = executingAssembly.GetName().Name;
-            AppDomain.CurrentDomain.AssemblyResolve += (sender, args) => {
-                                                           var resourceName = executingAssemblyName + ".DllsAsResource." + new AssemblyName(args.Name).Name + ".dll";
-                                                           using (var stream = executingAssembly.GetManifestResourceStream(resourceName)) {
-                                                               if (stream != null) {
-                                                                   var assemblyData = new Byte[stream.Length];
-                                                                   stream.Read(assemblyData, 0, assemblyData.Length);
-                                                                   return Assembly.Load(assemblyData);
-                                                               }
-                                                           }
-                                                           return null;
-                                                       };
-        }
 
         [STAThread]
         public static void Main()
