60a6f3e6b6aaf0bc4c70a9f7e7ac2064247d594f
Jan Karger <punkerat76@gmail.com>
2016-01-23 21:17:50 +0100

    simplify paket dependencies, update packages

--- SimpleMusicPlayer/Build.cmd
+++ SimpleMusicPlayer/Build.cmd
@@ -1,5 +1,11 @@
+@echo off
+
+IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
+
 @echo on
-call "%VS120COMNTOOLS%vsvars32.bat"
+
+.paket\paket.bootstrapper.exe
+REM .paket\paket.exe update
 
 msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Debug /p:platform=x86 /m /t:Clean,Rebuild
 msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform=x86 /m /t:Clean,Rebuild
