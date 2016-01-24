5ac63992b3c500423549fe5e9abf562e13074c56
Jan Karger <punkerat76@gmail.com>
2016-01-24 14:10:50 +0100

    `content: once` for Fody

--- SimpleMusicPlayer/Build.cmd
+++ SimpleMusicPlayer/Build.cmd
@@ -5,7 +5,7 @@ IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
 @echo on
 
 .paket\paket.bootstrapper.exe
-REM .paket\paket.exe update
+.paket\paket.exe update
 
 msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Debug /p:platform=x86 /m /t:Clean,Rebuild
 msbuild.exe /ToolsVersion:4.0 "SimpleMusicPlayer.sln" /p:StrongName=True /p:configuration=Release /p:platform=x86 /m /t:Clean,Rebuild
