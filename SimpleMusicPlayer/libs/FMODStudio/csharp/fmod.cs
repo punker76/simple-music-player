b1a4945c55de12924003089f2abc515e7e18bdef
punker76 <punkerat76@gmail.com>
2016-06-08 14:43:24 +0200

    FMOD 1.08.04

--- SimpleMusicPlayer/libs/FMODStudio/csharp/fmod.cs
+++ SimpleMusicPlayer/libs/FMODStudio/csharp/fmod.cs
@@ -16,7 +16,7 @@ namespace FMOD
     */
     public class VERSION
     {
-        public const int    number = 0x00010802;
+        public const int    number = 0x00010804;
 #if WIN64
         public const string dll    = "fmod64";
 #else
