513de4dcc61ffd6a3839cc52a946d057acafc4a7
Jan Karger <jan.karger@inform-software.com>
2017-07-26 08:24:09 +0200

    restore only

--- src/Build.cmd
+++ src/Build.cmd
@@ -5,7 +5,7 @@ IF NOT "%VS140COMNTOOLS%" == "" (call "%VS140COMNTOOLS%vsvars32.bat")
 @echo on
 
 .paket\paket.bootstrapper.exe
-.paket\paket.exe update
+.paket\paket.exe restore
 
 msbuild.exe /ToolsVersion:14.0 "SimpleMusicPlayer.sln" /p:configuration=Debug /p:platform=x86 /m /t:Clean,Rebuild
 msbuild.exe /ToolsVersion:14.0 "SimpleMusicPlayer.sln" /p:configuration=Release /p:platform=x86 /m /t:Clean,Rebuild
